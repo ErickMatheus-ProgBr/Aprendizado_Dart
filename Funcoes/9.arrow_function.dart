791c3fa2550615d1b7c1c032d07c8e137e8b2fd5
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-01-02 21:16:01 -0300

    ok

---
+++ Funcoes/9.arrow_function.dart
@@ -0,0 +1,26 @@
+// 📌 O que é Arrow Function?
+
+// Arrow function é uma forma curta de escrever funções que têm apenas uma expressão.
+// Em Dart, usamos =>.
+
+// void main() {
+//   final txt = getText(); //retorno é armazenado em txt
+//   print(txt); //imprime o valor
+// }
+
+// String getText() => "ERICK MATHEUS DA CRUZ SILVA"; //retorna uma string
+
+// TEMOS UMA OUTRA MANEIRA DE VER ESSA FUNÇÃO:::::
+
+// void main() {
+//   getTxt(); // não retorna nada
+// }
+
+// void getTxt() => print("Texto"); // o print acontece dentro da função
+
+// ==============================================================================
+
+// quando utlizar STRING e quando Utilizar VOID
+
+// String buscarMensagem() => "Olá";
+// void mostrarMensagem() => print("Olá");
